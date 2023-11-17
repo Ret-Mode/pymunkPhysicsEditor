652a43604e3ed94399482cf619aa7e7b30a36472
Ret-Mode <ernest.harcej@wp.pl>
2023-11-17 23:01:05 +0100

    Corrections to opengl loader (WIP)

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import array
 import arcade
-from arcade.gl import BufferDescription
+from arcade.gl import BufferDescription, Texture
 from arcade import ArcadeContext
 from pyglet.math import Mat4, Vec2, Vec3, Vec4
 import random
@@ -45,6 +45,18 @@ class Camera(arcade.Camera):
 class ArcadeProxy:
     def __init__(self, sprite, body, pos:Vec2, angle:float):
         self.sprite:arcade.Sprite = sprite
+        self.body:pymunk.Body = body
+        self.dPos = pos
+        self.dAngle = angle
+
+    def update(self):
+        self.sprite.radians = self.body.angle + self.dAngle
+        self.sprite.position = self.body.position + self.dPos.rotate(self.body.angle)
+
+
+class GLProxy:
+    def __init__(self, arrayIndex:int, body, pos:Vec2, angle:float):
+        self.arrayIndex = arrayIndex
         self.body:pymunk.Body = body
         self.dPos = pos
         self.dAngle = angle
@@ -195,6 +207,56 @@ class SpriteLoader(PymunkLoader):
     def draw(self):
         for proxy in self.proxy:
             proxy.sprite.draw()
+    
+    def debug(self):
+        for proxy in self.proxy:
+            print(proxy.body.position.x, proxy.body.position.y, proxy.body.angle)
+
+
+class GLLoader(PymunkLoader):
+    def __init__(self, space:pymunk.Space):
+        super().__init__(space)
+        self.proxy:List[GLProxy] = []
+        self.textures : Dict[str, Texture] = {}
+
+    def loadFile(self, path:str):
+        data = None
+        obj = None
+        with open(path, 'r') as f:
+            data = f.read()
+        if data:
+            obj = json.loads(data)
+            super().loadData(obj)
+            self.loadData(obj)
+
+    def loadData(self, obj:Dict):
+        self.loadGL(obj['Textures'], obj['Mappings'])
+
+    # TODO
+    def loadGL(self,textures, mappings):     
+        for channel, path in textures.items():
+            self.textures[channel] = arcade.get_window().ctx.load_texture(path)
+
+        for label, mapping in mappings.items():
+            channel = mapping['textureChannel']
+            texturePath = textures[channel]['path']
+            textureSize = textures[channel]['size']
+            offset = mapping['offset']
+            size = mapping['size']
+            scale = mapping['subScale']
+            body = self.bodies[mapping['body']]
+            rot = mapping['subRotate']
+            anchor = mapping['subAnchor']
+            self.proxy.append(GLProxy(0, body, Vec2(anchor[0], anchor[1]), rot))
+
+    def update(self):
+        for proxy in self.proxy:
+            proxy.body.angle += 0.01
+            proxy.update()
+
+    def draw(self):
+        for proxy in self.proxy:
+            pass
     
     def debug(self):
         for proxy in self.proxy:
